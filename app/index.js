7fbd6e503403c325a0f61d49983480734d0c3bf8
Jared Tibshraeny <jmtibs@gmail.com>
2017-07-09 18:55:40 -0700

    start hooking up view to some javascript

--- app/index.js
+++ app/index.js
@@ -1,2 +1,36 @@
-//TODO
-//javascript that operates on the dropdown window itself goes in here
+const {ipcRenderer, shell} = require('electron')
+
+document.addEventListener('click', (event) => {
+  if (event.target.href) {
+    // Open links in external browser
+    shell.openExternal(event.target.href)
+    event.preventDefault()
+  } else if (event.target.classList.contains('js-refresh-action')) {
+    updateLinks()
+  } else if (event.target.classList.contains('js-add-subscription-action')) {
+    addSubscription()
+  } else if (event.target.classList.contains('js-quit-action')) {
+    window.close()
+  }
+})
+
+function addSubscription() {
+  alert("adding subscription!")
+}
+
+function updateLinks() {
+ // fetch to api here to get list of links
+  links = []
+  // this communicates to the main process
+  // which can do things like display an icon with a nofitication by adjusting the tray (possible do different color for when a link has been updated, see weather example
+  ipcRenderer.send('links-updated', links)
+  updateView(links)
+  sendNotification(links)
+}
+
+function updateView(links) {
+  document.querySelector('.js-summary').textContent = "your lynx"
+}
+
+// Update links when loaded
+document.addEventListener('DOMContentLoaded', updateLinks)
