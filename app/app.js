98e1a535c3abc27cd5004160563e176b1ff274cb
Jared Tibshraeny <jmtibs@gmail.com>
2017-07-26 23:06:06 -0700

    start moving code over to react component

--- app/app.js
+++ app/app.js
@@ -7,7 +7,29 @@ class App extends React.Component {
           <div>
             <div className="header-arrow"></div>
             <div className="window">
-              <h1> hello </h1>
+              <header className="toolbar toolbar-header">
+                <h1 className="title">Lynx</h1>
+              </header>
+              <div className="window-content">
+                <div className="pane">
+                  <div className="summary js-summary">Loading&hellip;</div>
+                  <div id="links-container"></div>
+                </div>
+              </div>
+
+              <footer className="toolbar toolbar-footer">
+                <div className="toolbar-actions pull-left">
+                  <button className="btn btn-default js-refresh-action">
+                    <span className="icon icon-arrows-ccw js-refresh-action" title="Refresh"></span>
+                  </button>
+                </div>
+
+                <div className="toolbar-actions pull-right">
+                  <button className="btn btn-default js-add-subscription-action">
+                    <span className="icon icon-user-add js-add-subscription-action" title="Add Subscription"></span>
+                  </button>
+                </div>
+              </footer>
             </div>
           </div>
         );
