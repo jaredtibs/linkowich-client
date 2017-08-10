c617d7c753f7eba98be60dcba9e529027b260dc3
Jared Tibshraeny <jmtibs@gmail.com>
2017-08-09 23:00:40 -0700

    link form handler

--- app/components/Publish.js
+++ app/components/Publish.js
@@ -3,12 +3,39 @@ import React, { Component, PropTypes } from 'react';
 class Publish extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      url: ''
+    }
+  }
+
+  handleChange(event) {
+    this.setState({url: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.publishLink(this.state.url);
   }
 
   render() {
+
     return(
-      <div>
-        <p> this is where you publish </p>
+      <div className="publish-container">
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div className="form-group">
+            <input
+              type="url"
+              name="url"
+              placeholder="publish a link"
+              value={this.state.url}
+              onChange={this.handleChange.bind(this)}
+            />
+          </div>
+          <div className="form-actions">
+            <button type="submit" className="btn btn-form btn-primary"> Publish </button>
+          </div>
+        </form>
       </div>
     )
   }
