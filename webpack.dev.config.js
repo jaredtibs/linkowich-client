c62e7985015406b723b1503389fa68a954faff85
Jared Tibshraeny <jmtibs@gmail.com>
2017-08-28 22:53:35 -0700

    modify js pattern

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -26,7 +26,7 @@ module.exports = {
         include: defaultInclude
       },
       {
-        test: /\.jsx?$/,
+        test: /\.js?$/,
         use: [{ loader: 'babel-loader' }],
         include: defaultInclude
       },
