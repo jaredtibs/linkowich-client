d34798bbf4e474744d1fbd7375bef5144e3e5c7e
Jared Tibshraeny <jmtibs@gmail.com>
2017-09-04 21:07:39 -0700

    make header connected

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom';
 import styles from '../assets/stylesheets/header.css';
 
@@ -7,29 +8,51 @@ class Header extends Component {
     super(props)
   }
 
+  hasHeader(location) {
+    switch(location) {
+      case '/home':
+      case '/profile':
+      case '/settings':
+        return true;
+      default:
+        return false;
+    }
+  }
+
+
   render() {
-    return(
-      <div className="header">
-        <div className="header-section">
-          <Link to={'/settings'}>
-            <i className="material-icons settings">settings</i>
-          </Link>
+    let { location } = this.props.router;
+
+    if (this.hasHeader(location.pathname)) {
+      return(
+        <div className="header">
+          <div className="header-section">
+            <Link to={'/settings'}>
+              <i className="material-icons settings">settings</i>
+            </Link>
+          </div>
+          <div className="app-title header-section">
+            <Link to={'/home'}>
+              <span className="title-link">link</span>
+              <span className="title-o">o</span>
+              <span className="title-wich">wich</span>
+            </Link>
+          </div>
+          <div className="header-section">
+            <Link to={'/profile'}>
+              <i className="material-icons user">face</i>
+            </Link>
+          </div>
         </div>
-        <div className="app-title header-section">
-          <Link to={'/home'}>
-            <span className="title-link">link</span>
-            <span className="title-o">o</span>
-            <span className="title-wich">wich</span>
-          </Link>
-        </div>
-        <div className="header-section">
-          <Link to={'/profile'}>
-            <i className="material-icons user">face</i>
-          </Link>
-        </div>
-      </div>
-    )
+      )
+    } else {
+      return null;
+    }
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  return state;
+};
+
+export default connect(mapStateToProps)(Header);
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import {connect} from 'react-redux'
-import Header from '../components/Header'
+import ConnectedHeader from '../components/Header'
 import Footer from '../components/Footer'
 import styles from '../assets/stylesheets/app.css';
 
@@ -9,25 +8,12 @@ class App extends Component {
     super(props)
   }
 
-  hasHeader(location) {
-    switch(location) {
-      case '/home':
-      case '/profile':
-      case '/settings':
-        return true;
-      default:
-        return false;
-    }
-  }
-
   render() {
-    let { location } = this.props.router;
-
     return(
       <div>
         <div className="header-arrow"></div>
         <div className="window">
-          { this.hasHeader(location.pathname) ? <Header /> : null}
+          <ConnectedHeader />
           {this.props.children}
           <Footer />
         </div>
@@ -36,8 +22,4 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return state
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
