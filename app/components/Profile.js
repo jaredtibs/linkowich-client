3c662f3e3a3ddb27dd2dba814c2f91e0e7112110
Jared Tibshraeny <jmtibs@gmail.com>
2017-09-12 22:32:23 -0700

    wip user profile

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,15 +1,52 @@
 import React, { Component, PropTypes } from 'react';
+import styles from '../assets/stylesheets/profile.scss';
+import defaultAvatar from '../assets/default_avatar.jpeg';
 
 class Profile extends Component {
   constructor(props) {
     super(props)
   }
 
+  renderPastLinks() {
+      /*
+    let { links } = this.props.user;
+
+    if (links.length > 0) {
+      let linkList = links.map(function(link, i) {
+        return <FeedLink key={i} data={link} onClick={onClick} />
+      });
+
+      return(
+        <div> {linkList} </div>
+      )
+    } else {
+      return(
+        <div className="empty-feed-container">
+          <div className="empty-feed-text"> follow people and what they share will appear here </div>
+        </div>
+      )
+    }
+    */
+  }
+
   render() {
     return(
       <div className="window-content">
-        <div className="pane">
-          <span> Your Profile </span>
+        <div className="profile-container">
+          <div className="upper-container">
+            <div className="profile-avatar-container">
+              <img className="profile-avatar" src={defaultAvatar} />
+            </div>
+            <div className="profile-info-container">
+              <span className="profile-username">jaredtibs</span>
+            </div>
+          </div>
+          <div className="lower-container">
+            <span>Your Links</span>
+            <div className="past-links-container">
+              {this.renderPastLinks()}
+            </div>
+          </div>
         </div>
       </div>
     )
