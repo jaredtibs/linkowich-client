57a60434a2fa4199c8528cac7570d582fee1bea3
Jared Tibshraeny <jmtibs@gmail.com>
2017-09-20 00:10:01 -0700

    wip settings styles

--- app/components/Settings.js
+++ app/components/Settings.js
@@ -14,63 +14,71 @@ class Settings extends Component {
             <div className="section-subheader">
               <span>Account</span>
             </div>
-            <div className="settings-option">
-              <a className="section-btn" href="#">
-                upgrade to premium
-              </a>
-            </div>
-            <div className="settings-option">
-              <a className="section-btn" href="#">
-                change password
-              </a>
-            </div>
-            <div className="settings-option">
-              <a className="section-btn" href="#">
-                blocked users
-              </a>
+            <div className="options-container">
+              <div className="settings-option">
+                <a className="section-btn" href="#">
+                  upgrade to premium
+                </a>
+              </div>
+              <div className="settings-option">
+                <a className="section-btn" href="#">
+                  change password
+                </a>
+              </div>
+              <div className="settings-option">
+                <a className="section-btn" href="#">
+                  blocked users
+                </a>
+              </div>
             </div>
           </div>
           <div className="settings-section settings-support">
             <div className="section-subheader">
               <span>Support</span>
             </div>
-            <div className="settings-option">
-              <a className="section-btn" href="#">
-                FAQ
-              </a>
-            </div>
-            <div className="settings-option">
-              <a className="section-btn" href="#">
-                report a problem
-              </a>
+            <div className="options-container">
+              <div className="settings-option">
+                <a className="section-btn" href="#">
+                  FAQ
+                </a>
+              </div>
+              <div className="settings-option">
+                <a className="section-btn" href="#">
+                  report a problem
+                </a>
+              </div>
             </div>
           </div>
           <div className="settings-section settings-about">
             <div className="section-subheader">
               <span>About</span>
             </div>
-            <div className="settings-option">
-              <a className="section-btn" href="#">
-                website
-              </a>
-            </div>
-            <div className="settings-option">
-              <a className="section-btn" href="#">
-                privacy policy
-              </a>
-            </div>
-            <div className="settings-option">
-              <a className="section-btn" href="#">
-                terms
-              </a>
+            <div className="options-container">
+              <div className="settings-option">
+                <a className="section-btn" href="#">
+                  website
+                </a>
+              </div>
+              <div className="settings-option">
+                <a className="section-btn" href="#">
+                  privacy policy
+                </a>
+              </div>
+              <div className="settings-option">
+                <a className="section-btn" href="#">
+                  terms
+                </a>
+              </div>
             </div>
           </div>
           <div className="settings-section settings-logout">
-            <div className="settings-option">
-              <a className="bottom-btn" href="#">clear link history</a>
-            </div>
-            <div className="settings-option">
-              <a className="bottom-btn" href="#" onClick={() => this.props.logout() }>logout</a>
+            <div className="options-container bottom">
+              <div className="settings-option">
+                <a className="bottom-btn" href="#">clear link history</a>
+              </div>
+              <div className="settings-option">
+                <a className="bottom-btn" href="#" onClick={() => this.props.logout() }>logout</a>
+              </div>
             </div>
           </div>
         </div>
