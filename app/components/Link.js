1f464f25da04320755339f774498b01d9c03a74c
Jared Tibshraeny <jmtibs@gmail.com>
2017-09-29 00:08:16 -0700

    add link component reusability functionality wip

--- app/components/Link.js
+++ app/components/Link.js
@@ -37,34 +37,62 @@ class Link extends Component {
     this.setState({copied: false})
   }
 
+  renderFeedLinkHeader(link, user) {
+    let { avatar, username } = user;
+    let avatar_src = avatar.url ? avatar.url : defaultAvatar;
+
+    return(
+      <div className='link-row-header-container'>
+        <div className="link-meta-container">
+          <div className="avatar">
+            <img src={avatar_src} width={30} height={30} />
+          </div>
+          <div className="meta-text">
+            <span className='username'>{username}</span>
+            <span className='timestamp'>{link.attributes['published-at']} ago</span>
+          </div>
+        </div>
+
+        <div className="link-vote-container">
+          <i className="material-icons vote-icon">whatshot</i>
+        </div>
+      </div>
+    )
+  }
+
+  renderProfileLinkHeader(link, user) {
+    let { avatar, username } = user;
+    let avatar_src = avatar.url ? avatar.url : defaultAvatar;
+
+    return(
+      <div className='link-row-header-container'>
+        <div className="link-meta-container">
+          <div className="meta-text">
+            <span className='username'>{username}</span>
+            <span className='timestamp'>{link.attributes['published-at']} ago</span>
+          </div>
+        </div>
+
+        <div className="link-vote-container">
+          <i className="material-icons vote-icon">whatshot</i>
+        </div>
+      </div>
+    )
+  }
+
   render() {
     const link = this.props.data
     const seenBy = link.attributes['seen-by']
     const user = link.attributes.user.data.attributes
-    const { avatar, username } = user;
-    const avatar_src = avatar.url ? avatar.url : defaultAvatar;
 
     return(
       <div className='link-row'
            onMouseOut={this._handleParentHover.bind(this)}
       >
-        <div className='link-row-header-container'>
-
-          <div className="link-meta-container">
-            <div className="avatar">
-              <img src={avatar_src} width={30} height={30} />
-            </div>
-            <div className="meta-text">
-              <span className='username'>{username}</span>
-              <span className='timestamp'>{link.attributes['published-at']} ago</span>
-            </div>
-          </div>
-
-          <div className="link-vote-container">
-            <i className="material-icons vote-icon">whatshot</i>
-          </div>
-        </div>
-
+        { this.props.profile ?
+          this.renderProfileLinkHeader(link, user)
+          : this.renderFeedLinkHeader(link, user)
+        }
         <div className='link-container'
              onMouseOver={this._handleHover.bind(this)}
              onMouseOut={this._handleHover.bind(this)}>
