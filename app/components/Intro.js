a225d7a69d207cb95cb5ae85d4bcc07f2e37c18c
Jared Tibshraeny <jmtibs@gmail.com>
2017-10-05 23:16:37 -0700

    wip using custom react-motion solution over library

--- app/components/Intro.js
+++ app/components/Intro.js
@@ -1,12 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../assets/stylesheets/intro.scss';
-import TextLoop from 'react-text-loop';
+import {Motion, spring} from 'react-motion';
 const introIcon = require('../assets/intro_icon.svg');
 
 class Intro extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      headerCopy: "SHARE SOME MUSIC"
+    }
+  }
+
+  handleAnimationLeave() {
+    console.log("animation over")
   }
 
   render() {
@@ -15,13 +23,9 @@ class Intro extends Component {
         <div className="content-container">
           <img className="icon" src={introIcon} width={46} height={56} />
           <div className="copy-container">
-            <TextLoop style={{width: '156px'}}>
-              <span className="copy-text">SHARE SOME MUSIC</span>
-              <span className="copy-text">SHARE SOME MEMES</span>
-              <span className="copy-text">SHARE SOME NEWS</span>
-              <span className="copy-text padded">SHARE ANYTHING</span>
-              <span className="copy-text padded">SHARE SOME FIRE</span>
-            </TextLoop>
+            <Motion defaultStyle={{ width: 0 }} style={{width: spring(10)}} onRest={this.handleAnimationLeave.bind(this)} >
+              {style => <div className="copy-text" style={style}> {this.state.headerCopy} </div>}
+            </Motion>
           </div>
         </div>
       </div>
