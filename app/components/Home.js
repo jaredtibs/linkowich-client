cd93b4c630f84e9a12f1eb5729e4fd55f56cd420
Jared Tibshraeny <jmtibs@gmail.com>
2017-11-05 20:26:08 -0800

    pusher config and functions

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,37 @@
+const { ipcRenderer } = window.require('electron');
+
 import React, { Component, PropTypes } from 'react';
 import ShareContainer from '../containers/ShareContainer'
 import FeedContainer from '../containers/FeedContainer'
+import Pusher from 'pusher-js';
 
 class Home extends Component {
   constructor(props) {
     super(props)
+  }
+
+  componentWillMount() {
+    this.pusher = new Pusher("a138d00997c28c8aad6b", {
+      cluster: "us2",
+      encrypted: true
+    });
+
+    this.channel = this.pusher.subscribe('links');
+  }
+
+  componentDidMount() {
+    this.channel.bind('link-published', this.handleLinkPublished);
+  }
+
+  componentWillUnmount() {
+    this.channel.unbind();
+    this.pusher.unsubscribe(this.channel);
+  }
+
+  handleLinkPublished(data) {
+    const { message } = data;
+    console.log(message);
+    ipcRenderer.send('new-link-published');
   }
 
   render() {
